10d4514076dbc663a470e577d9d883838d96d998
Daniel Ik <codingeinstein@gmail.com>
2022-06-26 07:44:05 +0100

    added linear layer

--- src/activations.js
+++ src/activations.js
@@ -139,4 +139,73 @@ Sigmoid.prototype = {
 };
 
 
-export {ReLU, Sigmoid};
+function Linear(){
+
+};
+
+Linear.prototype = {
+    forward: function(x){
+        this.out = typeof x == "number"? 0: [];
+
+        if(this.out == 0){
+
+            this.out = this.forward_func(x);
+        
+        }else{
+            for(let i=0; i<x.length;i++){
+
+                this.out.push(this.forward_func(x[i]));
+
+            };
+        };
+
+        return this.out;
+
+    },
+
+    backward: function(grad){
+
+        // incoming grad is the same shape as the activation output
+
+        if (typeof grad == "number"){
+
+            this.grad = grad*this.backward_func(this.x);
+            return this.grad;
+
+        }else{
+
+            this.grad = [];
+
+            for (let i=0; i<grad.length; i++){
+
+                this.grad[i] = grad[i]*this.backward_func(this.x[i]);
+            
+            };
+        };
+
+    },
+
+
+    forward_func: function(x){
+        return x;
+
+    },
+
+    backward_func: function (x){
+
+        return 1;
+
+    },
+
+    zero_grad: function(){
+        if (typeof this.grad == "number"){
+
+            this.number = 0; 
+        }else{
+            this.grad.fill(0);
+        };
+    },
+};
+
+
+export {ReLU, Sigmoid, Linear};
