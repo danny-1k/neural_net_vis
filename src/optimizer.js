19f18628c18207613080350d8141d136dcbe5b16
Daniel Ik <codingeinstein@gmail.com>
2022-07-09 10:31:22 +0100

    added optimizer.js

---
+++ src/optimizer.js
@@ -0,0 +1,28 @@
+function Optimizer(net,lr){
+
+    this.net = net;
+    this.lr = lr;
+    
+
+
+};
+
+
+Optimizer.prototype = {
+
+    step: function(){
+
+    },
+
+    zero_grad: function(){
+
+    },
+
+    
+
+}
+
+
+
+
+export default Optimizer;
